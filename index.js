3e3aaa4443f7b463bf15b6ed27908fd01e72be5c
Prat2030 <pratyushjindal2003@gmail.com>
2023-04-14 19:19:59 +0530

    created user schema and imported few npm packages

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const port = 4000;
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+dotenv.config();
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT,)
+    .then(() =>
+            console.log('Connected to database!')
+        )
+    .catch((err) => console.log(err));
+
+// Import Routes
+const authRoute = require('./routes/auth');
+
+// Route Middlewares
+app.use('/api/user', authRoute);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+### JWT Authentication API Basics
+
+- This is a simple API that uses JWT authentication. It is meant to be used as a starting point for a project that requires authentication.
