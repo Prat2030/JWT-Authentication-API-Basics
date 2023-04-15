30750ee2f4e1b18ba24d2a5e5e96bab692a6b7f0
Prat2030 <pratyushjindal2003@gmail.com>
2023-04-16 01:59:33 +0530

    created a jwt tokens for private routes

--- readme.md
+++ readme.md
@@ -5,4 +5,4 @@
 - Using ExpressJs as bodyparser to get the data as Json format.
 - Using Joi npm package for validation. (use "npm i joi" to install it).
 - Using bcryptjs npm package for hashing the password. (use "npm i bcryptjs" to install it).
-- 
+- Using jsonwebtoken npm package for generating the token. It creates a token and we store and just show it to tell that we are already logged in So, do not need to login again and again.(use "npm i jsonwebtoken" to install it).
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const {registerValidation} = require('../validation');
 
 router.post('/register', async (req, res) => {
@@ -44,6 +45,10 @@ router.post('/login', async (req, res) => {
     // Password is correct
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if (!validPass) return res.status(400).send('Invalid password');
+
+    // Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
 
     res.send('Logged in!');
 });
